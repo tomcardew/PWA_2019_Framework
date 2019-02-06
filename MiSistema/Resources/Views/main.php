4b85454b10ef14633d1c809446cb6f4c19f17578
tomcardew <tomcar97@gmail.com>
2019-02-05 20:17:00 -0600

    Agregada carpeta estilos

--- MiSistema/Resources/Views/main.php
+++ MiSistema/Resources/Views/main.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 	<title>Principal</title>
+	<style>
+		<?php include_once '/../Styles/style.css' ?>
+	</style>
 </head>
-<body style="background: linear-gradient(to right, rgba(0,255,0,0), rgba(0,255,200,1));">
-	<h1 style="color: #888;">SuperKroud</h1>
+<body>
+	<h1>SuperKroud</h1>
 	<p><h4>Bienvenido</h4></p>
 	<p>Esta es la página principal de tu nueva página web. Puedes añadir nuevas vistas desde el código fuente.</p>
 </body>
